7557c47bf20846a4b7adbf6c459e5b788dc93a87
phil <uniphil@gmail.com>
2016-05-31 11:41:43 -0400

    Add an n-counters example

---
+++ examples/n-counters/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NCounters from './n-counters';
+
+ReactDOM.render(<NCounters />, document.getElementById('app'));
---
+++ examples/n-counters/n-counters.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Immutable, { Record } from 'immutable';
+import { Union } from 'results';
+import { component, Update } from '../../spindle';
+import Counter from '../counter/counter';
+
+
+const Model = Record({
+  counters: Immutable.List([]),
+});
+
+
+const Msg = Union({
+  Add: null,
+  Remove: null,
+});
+
+
+const update = (msg, model) => Msg.match(msg, {
+  Add: () =>
+    Update({ model: model.update('counters', list => list.push(list.size)) }),
+
+  Remove: () =>
+    Update({ model: model.update('counters', list => list.pop()) }),
+});
+
+
+const view = (model, BoundMsg) => (
+  <div>
+    <p>all the counters you could want...</p>
+    <p>
+      <button onClick={BoundMsg.Add}>Add another</button>
+      <button onClick={BoundMsg.Remove}>Remove one</button>
+    </p>
+    {model.get('counters').map(id => (
+      <Counter key={id} />
+    )).toArray()}
+  </div>
+);
+
+
+export default component('NCounters',
+  { Msg, Model, update, view });
